073dde7196b7ce6548b9359c3a50a7d1df646df7
Oxii_FW_Reviewer <long.nh@oxiitek.com>
2026-05-01 16:43:32 +0700

    Làm tiếp bài tập về nhà buổi 3

--- IMIC_220621/BTVN_Buoi3.c
+++ IMIC_220621/BTVN_Buoi3.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "stdio.h"
+#include "math.h"
 
 void bai3_1_1(void)
 {
@@ -197,4 +198,236 @@ void bai3_3_2(void)
 	}
 	int bcln = (x * y) / a;
 	printf("Boi chung lon nhat la: %d\n", bcln);
+}
+
+void bai3_3_3(void)
+{
+	int n = 0;
+	printf("Nhap n: ");
+	scanf("%d", &n);
+	int sum = 0;
+	if (n <= 0)
+	{
+		printf("%d khong phai so hoan hao \n");
+		return;
+	}
+	for (int i = 1; i <= n / 2; i++)
+	{
+		if (n % i == 0)
+		{
+			sum += i;
+		}
+	}
+	if (sum == n)
+	{
+		printf("%d la so hoan hao \n", n);
+	}
+	else
+	{
+		printf("%d khong phai so hoan hao \n", n);
+	}
+}
+
+void bai3_3_4(void)
+{
+	int n = 0;
+	printf("Nhap n: ");
+	scanf("%d", &n);
+	int isPrime = 0;
+	if (n < 2)
+	{
+		return;
+	}
+	printf("Cac so nguyen to la: \n");
+	for (int i = 2;i <= n;i++)
+	{
+		isPrime = 1;
+		for (int j = 2; j <= sqrt(i); j++)
+		{
+			if (i % j == 0)
+			{
+				isPrime = 0;
+				break;
+			}
+		}
+		if (isPrime == 1)
+		{
+			printf("%d \n", i);
+		}
+	}
+}
+
+void bai3_3_5(void)
+{
+	int n = 0;
+	int a = 1, b = 1, c = 0;
+	printf("Nhap n: ");
+	scanf("%d", &n);
+	if (n == 1)
+	{
+		printf("%d ", a);
+	}
+	else if (n <= 0)
+	{
+		return;
+	}
+	else if (n >= 2)
+	{
+		printf("%d %d ", a, b);
+	}
+	for (int i = 3; i <= n; i++)
+	{
+		c = a + b;
+		printf("%d ", c);
+		a = b;
+		b = c;
+		
+	}
+}
+
+void bai3_3_6(void)
+{
+	int n = 0;
+	float sum = 0;
+	printf("Nhap n: ");
+	scanf("%d", &n);
+	for (int i = 0; i <= n - 1; i++)
+	{
+		int mau = 2 * i + 1;
+		float term = 1.0 / mau;
+		if (i % 2 == 0)
+		{
+			sum += term;
+		}
+		else
+		{
+			sum -= term;
+		}
+	}
+	float pi = 4 * sum;
+	printf("Ket qua = %.2f", pi);
+}
+
+void bai3_4_1(void)
+{
+	int w = 0, h = 0;
+	printf("Nhap w va h: ");
+	scanf("%d %d", &w, &h);
+	for (int i = 1; i <= h; i++)
+	{
+		for (int j = 1; j <= w; j++)
+		{
+			printf(".");
+		}
+		printf("\n");
+	}
+}
+
+void bai3_4_2(void)
+{
+	int canh = 0;
+	printf("canh = ");
+	scanf("%d", &canh);
+	for (int i = 0; i <= canh-1; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			printf(".");
+		}
+		printf("\n");
+	}
+}
+
+//void test(void)
+//{
+//	int canh = 0;
+//	printf("canh = ");
+//	int s = 0;
+//	scanf("%d", &canh);
+//	for (int i = 0; i < canh-1; i++)
+//	{
+//		printf(".");
+//		printf("\n");
+//		for (int j = 0; j <= s; j++)
+//		{
+//			printf(".");
+//		}
+//		s++;
+//	}
+//	printf(".");
+//}
+
+//void bai3_4_3_test(void)
+//{
+//	int canh = 0;
+//	printf("canh = ");
+//	int count = 0;
+//	scanf("%d", &canh);
+//	for (int i = 0; i < canh; i++)
+//	{
+//		for (int j = canh; j > i+1; j--)
+//		{
+//			printf(" ");
+//		}
+//		for (int k = 0; k <= count; k++)
+//		{
+//			printf(".");
+//		}
+//		count++;
+//		printf("\n");
+//	}
+//}
+
+void bai3_4_3(void)
+{
+	int canh = 0;
+	printf("canh = ");
+	scanf("%d", &canh);
+	for (int i = 1; i <= canh; i++)
+	{
+		for (int j = 1; j <= canh - i + 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+
+	}
+}
+
+void bai3_4_4(void)
+{
+	int a = 0;
+	printf("a = ");
+	scanf("%d", &a);
+	for (int i = 1; i <= a; i++)
+	{
+		for (int j = 1; j <= a - i; j++)
+		{
+			printf(" ");
+		}
+		for (int k = 1; k <= 2 * i - 1; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	for (int y = 1; y < a * 2; y++)
+	{
+		printf("*");
+	}
+	printf("\n");
+
+	for (int x = 1; x <= a; x++)
+	{
+		for (int y = 1; y <= x; y++)
+		{
+			printf(" ");
+		}
+		for (int z = 1; z < (a - x) * 2; z++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
 }
